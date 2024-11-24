ee9f7b081e51ab59507e3c52230932be252f2f93
97252 <dnhijazi15108@gmail.com>
2024-11-24 23:06:08 +0200

    first commit

--- app/src/main/java/com/example/app4/MainActivity.java
+++ app/src/main/java/com/example/app4/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.app4;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +12,51 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements  View.OnClickListener {
+    private EditText etNum1, etNum2;
+    private TextView tvResult;
+    private Button btnPlus, btnMinus,btnMultiply,btnDivision;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        etNum1 = findViewById(R.id.etNum1);
+        etNum2 = findViewById(R.id.etNum2);
+        tvResult = findViewById(R.id.tvResult);
+        btnPlus = findViewById(R.id.btnPlus);
+        btnPlus.setOnClickListener(this);
+        btnMinus = findViewById(R.id.btnMinus);
+        btnMinus.setOnClickListener(this);
+        btnMultiply = findViewById(R.id.btnMultiply);
+        btnMultiply.setOnClickListener(this);
+        btnDivision = findViewById(R.id.btnDivision);
+        btnDivision.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        int num1, num2;
+        if (!(etNum1.getText().toString().isEmpty() || etNum2.getText().toString().isEmpty())) {
+            num1 = Integer.parseInt(etNum1.getText().toString());
+            num2 = Integer.parseInt(etNum2.getText().toString());
+            if (view.getId() == R.id.btnPlus) {
+                tvResult.setText(String.valueOf(num1 + num2));
+            }
+            if (view.getId() == R.id.btnMinus) {
+                tvResult.setText(String.valueOf(num1 - num2));
+            }
+            if (view.getId() == R.id.btnMultiply) {
+                tvResult.setText(String.valueOf(num1 * num2));
+            }
+            if (view.getId() == R.id.btnDivision) {
+                tvResult.setText(String.valueOf(num1 / num2));
+            }
+
+        }
+        else{
+            tvResult.setText("Please enter numbers");
+        }
     }
 }
